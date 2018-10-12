2028bcd7b6b4c0639de11472320f9c0d1e617174
Miroshni4enko <miroshni4encko.slavikkk@yandex.ru>
2018-10-12 13:48:58 +0300

    data types

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,18 @@
 fun main(args: Array<String>) {
-    val userJava = UserJava("java_email")
-    println("1) ${userJava.email}")
-    val userKotlin = UserKotlin("kotlin_email")
-    println("2) ${userKotlin.email}")
+    var a0: Byte = 1  // 8
+    var a1: Short = 1 // 16
+    var a2: Int = 1   // 32
+    var a3: Long = 1  // 64
+
+    var a4: Float = .1F  // 32
+    var a5: Double = .1  // 64
+
+    var ex :Long = a2.toLong()
+
+    var c1: Char = 'b'
+    var c2: String = "bla"
+
+    var b1: Boolean = true
+    var b2: Boolean = false
+
 }
--- src/UserKotlin.kt
+++
@@ -1,1 +0,0 @@
-class UserKotlin(val email: String)
