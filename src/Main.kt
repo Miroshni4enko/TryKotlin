d92e56b371ea02517bda81a8e2bd6b8be9959474
Miroshni4enko <miroshni4encko.slavikkk@yandex.ru>
2018-10-12 13:14:47 +0300

    String Templates

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,7 @@
 fun main(args: Array<String>) {
-    println("Hello Kotlin")
+    var name = "Slava"
+    println("Hello $name! It's my first programme")
+    var a = 1
+    var b = 1
+    println("A + B = ${a + b}")
 }
