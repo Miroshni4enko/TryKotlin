6e3f426c46bbd585b2a0a32aead12ee21d1d51d0
Miroshni4enko <miroshni4encko.slavikkk@yandex.ru>
2018-10-12 18:06:25 +0300

    functions

--- src/Main.kt
+++ src/Main.kt
@@ -1,18 +1,14 @@
 fun main(args: Array<String>) {
-    var a0: Byte = 1  // 8
-    var a1: Short = 1 // 16
-    var a2: Int = 1   // 32
-    var a3: Long = 1  // 64
-
-    var a4: Float = .1F  // 32
-    var a5: Double = .1  // 64
-
-    var ex :Long = a2.toLong()
-
-    var c1: Char = 'b'
-    var c2: String = "bla"
-
-    var b1: Boolean = true
-    var b2: Boolean = false
-
+    mySecondFun(myFirstFun().toLong())
+    myThirdFun(1, .2)
 }
+
+fun myFirstFun(): Int {
+    return 1
+}
+
+fun mySecondFun(arg: Long): Int {
+    return arg.toInt()
+}
+
+fun myThirdFun(a: Long, b: Double) = a + b
