cd9f4bf2b882fa5a7c512c7d72adf17464b58777
Miroshni4enko <miroshni4encko.slavikkk@yandex.ru>
2018-10-16 20:53:47 +0300

    enum

---
+++ src/Color.kt
@@ -0,0 +1,6 @@
+enum class Color(val colorHex:String, val opacity: Double? = 1.0) {
+    WHITE("#white", 2.3),
+    BLACK("#black"),
+    RED("#red"),
+    GREEN("#green")
+}
--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,7 @@
 fun main(args: Array<String>) {
-    val person = Person("Slavik", "Mirosh", 123)
-    println(person)
-
-    val car = Car(123.4F, 213)
-    println(car.isNew)
-
+    var message : Color = Color.BLACK
+    println(message)
+    println(message.colorHex)
+    println(message.opacity)
+    println(message.name)
 }
